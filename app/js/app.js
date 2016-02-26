8d9a19d37cc540d7a2bc7074a499ac7f92ac5982
Mark Carpenter Jr <marksthings24@gmail.com>
2016-02-25 23:43:21 -0500

    Update: resolved commit diffs
    Resolved sync issues on other dev enviroments.

--- app/js/app.js
+++ app/js/app.js
@@ -11,7 +11,7 @@ function model() {
    * @param business_id: string containing the Yelp business ID
    */
 
-  self.defLocations = [{
+  self.default = [{
     "name": "1900' Burger",
     "loc": [42.967257, -72.894326, 17],
     "icon": "img/glyphs/gm-beer.svg",
@@ -81,70 +81,64 @@ function model() {
 
   // Set the home location coordinates to initialize the map here
   self.defLoc = [42.9557093, -72.8923977, 14];
-
   // Create an empty array to store a list of map markers
   self.markers = [];
   self.infoWindows = [];
-
   // Empty Variable for GeoLocation
   self.currentLoc = [];
 }
 var model = new model();
 
-//-------------------------------------------------------------------------
+//***************************************************************//
 
 function appVM() {
   var self = this;
   // Foursquare ID and Secret Token for the api.
   var FSCLIENT_ID = 'SCCAY03SWJPAHUTNJNEDCXXHHQC0MNPZFJGZCLIPXGRUVCLC';
   var FSCLIENT_SECRET = '020SLKZRVCSZK3BWLXUNHMB0DF5DA21XQQSHWH1DSN5D5QYQ';
-    // Map Marker Variable.
+  // Map Marker Bouncing Variable, Null Stops premature Bouncing.
   var markerBouncing = null;
-  // InfoWindow Variable
+  // InfoWindow Variable, starts empty.
   var openInfoWindow = null;
-  // Marker and InfoWindow Content Variable and Array.
+  // InfoWindow Content Variable, Starts Empty.
   var infoWindowHTML = '';
-  self.infoWindowHTMLs = [];
+  // Default Zoom Level
+  self.defZoom = model.defLoc[2];
+  // Default View center, converted to Google Maps LatLng.
+  self.defCenter = {
+    lat: model.defLoc[0],
+    lng: model.defLoc[1]
+  };
 
-  // Search term empty Variable.
+  // Search, Empty Variables and Array.
   self.searchQuery = ko.observable("");
   self.searchResults = ko.observableArray([]);
 
+  //***************************************************************//
+
   /**
-   * @description Hard Coded locations
-   * @param name: string holding the locations name
-   * @param lat: the lattitude of the location
-   * @param lng: the longitude of the location
-   * @param icon: string holding the icon path typically img/glyphs/*.svg or *.png
-   *         ideally the icon would be no more than 50px X 65px
-   * @param venue_id: string containing the fourSquare venue ID
-   * @param business_id: string containing the Yelp business ID
+   * @description Location List, Loops through Object pushes
+   *  the name endpoint to an array, for display, pushes a lowercase version
+   * to another array, to become searchable.
+   * @param locs: Object-s, holding JS data,
    */
 
-  self.dispResults = function(defLocations) {
-    self.dispResultsList = [];
+  self.locList = function(locs) {
+    self.locListItem = [];
     self.searchables = [];
-    for (i = 0; i < model.defLocations.length; i++) {
-      var item = model.defLocations[i].name;
-      self.dispResultsList.push(item);
+    for (i = 0; i < locs.length; i++) {
+      var item = locs[i].name;
+      self.locListItem.push(item);
       self.searchables.push(item.toLowerCase());
       console.log('Default Names Have Been Pushed');
     }
-    self.results = ko.observableArray(self.dispResultsList.slice(0));
+    self.results = ko.observableArray(self.locListItem.slice(0));
   };
   // Invokes initResults function on our locations.
-  self.dispResults(model.defLocations);
+  self.locList(model.default);
 
-  /**
-   * @description Hard Coded locations
-   * @param name: string holding the locations name
-   * @param lat: the lattitude of the location
-   * @param lng: the longitude of the location
-   * @param icon: string holding the icon path typically img/glyphs/*.svg or *.png
-   *         ideally the icon would be no more than 50px X 65px
-   * @param venue_id: string containing the fourSquare venue ID
-   * @param business_id: string containing the Yelp business ID
-   */
+  //***************************************************************//
+  // Search Function. . .
 
   self.searchF = function() {
     self.results.removeAll();
@@ -154,7 +148,7 @@ function appVM() {
     }
     self.searchables.forEach(function(item, index, array) {
       if (item.indexOf(self.searchQuery().toLowerCase()) > -1) {
-        self.results.push(self.dispResultsList[index]);
+        self.results.push(self.locListItem[index]);
 
         model.markers[index].setVisible(true);
       }
@@ -162,7 +156,7 @@ function appVM() {
 
     //If the filter input is empty, resets all locations to be visible
     if (self.searchQuery() === '') {
-      self.results(self.dispResultsList.slice(0));
+      self.results(self.locListItem.slice(0));
       model.markers.forEach(function(item, index, array) {
         if (!item.getVisible()) {
           item.setVisible(true);
@@ -176,19 +170,20 @@ function appVM() {
     if (openInfoWindow) openInfoWindow.close();
     if (markerBouncing) markerBouncing.setAnimation(null);
     self.searchF();
-    self.map.panTo(self.homelatlng);
-    self.map.setZoom(model.defLoc[2]);
+    self.map.panTo(self.defCenter);
+    self.map.setZoom(self.defZoom);
   };
 
   //***************************************************************//
+  // Map Function
 
-  // Begining of our google map functions.
+  // Begining of our google map functions. SPAGHETTI CODE. YAYAYAY!
   function dispMap(latlng) {
     var marker;
     var mapDisp = document.getElementById('map');
     var gLatLng = latlng;
     var mapOptions = {
-      zoom: model.defLoc[2],
+      zoom: self.defZoom,
       mapTypeId: google.maps.MapTypeId.HYBRID,
       center: gLatLng,
       disableDefaultUI: true
@@ -196,14 +191,9 @@ function appVM() {
     var map = new google.maps.Map(mapDisp, mapOptions);
     return map;
   }
-
-  // Default Location Based off the data in the Model(appM).
-  self.defLatLng = new google.maps.LatLng(model.defLoc[0], model.defLoc[1]);
-
   // Fires Up the Map.
-  self.map = dispMap(self.defLatLng);
+  self.map = dispMap(self.defCenter);
 
-  // Places our Map markers
   function addMarker(map, latlng, title, content, icon) {
     var markerOptions = {
       position: latlng,
@@ -213,21 +203,17 @@ function appVM() {
       clickable: true,
       icon: icon,
     };
-
     var marker = new google.maps.Marker(markerOptions);
     marker.addListener('click', function() {
       map.setZoom(17);
       map.setCenter(marker.getPosition());
     });
-
     var infoWindowOptions = {
       content: content,
       position: latlng
     };
-
     var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
     model.infoWindows.push(infoWindow);
-
     google.maps.event.addListener(marker, 'click', function() {
       if (openInfoWindow) openInfoWindow.close();
       openInfoWindow = infoWindow;
@@ -235,8 +221,8 @@ function appVM() {
       toggleBounce();
     });
     google.maps.event.addListener(infoWindow, 'closeclick', function() {
-      map.setZoom(model.defLoc[2]);
-      map.setCenter(self.defLatLng);
+      map.setZoom(self.defZoom);
+      map.setCenter(self.defCenter);
       $('.button-collapse').sideNav('show');
       toggleBounce();
     });
@@ -274,56 +260,57 @@ function appVM() {
     for (var i = 0; i < data.length; i++) {
       var location = data[i];
       var gMapLatLong = new google.maps.LatLng(location.loc[0], location.loc[1]);
+      // SETS INFO WINDOW CONTENT
       var windowContent = location.name;
       //creates Marker, Adds to map
-      // we need to add this once we find PNG or SVG Icons (location.icon)
       var marker = addMarker(self.map, gMapLatLong, location.name, windowContent, location.icon);
       // Makrers to Data Model
       model.markers.push(marker);
       console.log('Pushed New Markers');
     }
   };
+  self.initMap(model.default);
+
+  //***************************************************************//
+  // API CALL
 
   // Our Error Message timer... If fourSquare does not return any results
   // WE RIOT... J/K We only riot if they kill Daryl... We'll just throw an error.
   self.mesTimer = setTimeout(function() {
     Materialize.toast("Having trouble connecting, Try again...", 6000);
   }, 6000);
-
   // Api call to FourSquare, to start pulling Data.
   // If we wanted to get trick, maybe we could put this in a
   // service worker one day.
-
   self.fsApiCall = function(input) {
     for (var i = 0; i < input.length; i++) {
       var url = "https://api.foursquare.com/v2/venues/" +
-        model.defLocations[i].venue_id +
+        input[i].venue_id +
         "?client_id=" + FSCLIENT_ID +
         "&client_secret=" + FSCLIENT_SECRET +
         "&v=20151220";
 
       $.getJSON(url, function(data) {
-        clearTimeout(appVM.mesTimer);
+        clearTimeout(self.mesTimer);
         model.infoWindows.forEach(function(item, index, array) {
           if (item.content == data.response.venue.name) {
             infoWindowHTML = "<p><strong><a class='place-name' href='" + data.response.venue.canonicalUrl + "'>" + data.response.venue.name + "</a></strong></p>" + "<p>" + data.response.venue.location.address +
               "</p><p><span class='place-rating'><strong>" + data.response.venue.rating + "</strong><sup> / 10</sup></span>" + "<span class='place-category'>" + data.response.venue.categories[0].name + "</p><p>" + data.response.venue.hereNow.count + " people checked-in now</p>" + "<img src='" + data.response.venue.photos.groups[0].items[0].prefix + "80x80" + data.response.venue.photos.groups[0].items[0].suffix + "'</img>";
             item.setContent(infoWindowHTML);
-            console.log('Info Window Content Good');
-          } else {
-            console.log('Name MisMatch');
+            console.log('Info Window Content Good', data);
           }
         });
       });
     }
   };
-
+  self.fsApiCall(model.default);
 
   //***************************************************************//
-  // Our Wether Function
+  // Weather Function
 
   // Weather API Key
   var WEATHER_KEY = '6dc4f9aa6decdedf1ef5aab972c8471f';
+  // Weather Data Layout. . . ooOOH ObServables OOooh
   self.weatherData = {
     'current': {
       'id': ko.observable(''),
@@ -336,11 +323,11 @@ function appVM() {
     'wind': ko.observable(''),
     'good': ko.observable(false)
   };
-  self.weatherReport = function(weatherData) {
+  // Function To Retreive The Weather
+  self.weatherReport = function(location) {
     var url = "http://api.openweathermap.org/data/2.5/weather?" +
-      "lat=" + model.defLoc[0] + "&lon=" + model.defLoc[1] +
+      "lat=" + location[0] + "&lon=" + location[1] +
       "&units=imperial&APPID=" + WEATHER_KEY;
-
     $.ajax({
       url: url,
       type: 'GET',
@@ -354,52 +341,34 @@ function appVM() {
         appVM.weatherData.clouds(result.clouds.all);
         appVM.weatherData.wind(result.wind.speed);
         appVM.weatherData.good(true);
-
         $.extend(true, appVM.weatherData.current, {
           iconCode: result.weather[0].icon
         });
-
         console.log('OpenWeather.org Data:', result);
       },
       error: function(result) {
-        Materialize.toast("Can't Get Weather. . .", 6000)
+        Materialize.toast("Can't Get Weather. . .", 6000);
       }
     }).done(function() {
-      var weatherIcon = '<img src="http://openweathermap.org/img/w/' + self.weatherData.current.iconCode + '.png"></img>';
-      $('#weather').append(weatherIcon);
+      var weatherIcon = 'http://openweathermap.org/img/w/' + self.weatherData.current.iconCode + '.png';
+      $('#weatherIcon').attr("src", weatherIcon);
+      Materialize.toast("Weather Updated", 6000);
     });
   };
-  // This fires off the initMap function, setting the markers from the model.
-  self.initMap(model.defLocations);
-  // This runs the foursquare API call and appends the data returned to the
-  // map markers.
-  self.fsApiCall(model.defLocations);
-  self.weatherReport();
-
+  self.weatherReport(model.defLoc);
 }
 
-//------------------------------------------------------------------------
+//***************************************************************//
 
 // Gets some visuals ready when the app first loads.
-var init = function() {
+var initDisp = function() {
   $(document).ready(function() {
-
     $('.button-collapse').sideNav({
       menuWidth: 280, // Default is 240
       edge: 'left', // Choose the horizontal origin
       closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
     });
-
   });
-
-
-  $(document)
-    .ajaxStart(function() {
-      // Stuff for the start of the ajax call Goes Here
-    })
-    .ajaxStop(function() {
-      // Stuff for after the ajax call goes here
-    });
   swal({
     title: "Loading. . .",
     text: '<div class="progress"><div class="indeterminate"></div></div>' +
@@ -421,7 +390,10 @@ var init = function() {
     timer: 8500,
   });
 };
+function initApp() {
 
+}
 var appVM = new appVM();
-init();
 ko.applyBindings(appVM);
+
+initDisp();
